cbf4fd8c113175dbfd87cbacdc33ae437176d944
github username <github email address>
2018-10-31 12:27:50 -0400

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,26 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  // Write your JavaScript code inside the init() function
+  let index = 0;
 
+  document.body.addEventListener('keydown', function(e){
+
+    const key = parseInt(e.which || e.detail || e.location);
+
+      // console.log('w', e.which, 'd', e.detail, "l", e.location);
+    if (code[index] === key)
+
+      {index ++;
+
+      if (index === code.length) {
+      alert("You Got It!");
+
+      index = 0;
+  }
+
+} else {
+  index = 0;
 }
+});
+
+};
